49ed38ba810dbc60eb9c4692180fe76a72b18e2d
cid4si <cid4si@outlook.com>
2026-03-07 20:35:25 -0300

    update data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Configurações inicais
 });
+
+const DATA = {
+  name: "AĐΞMAR CIĐASI",
+  bio: "Fã de open source, e emocore.",
+  portfolio: "https://www.behance.net/cid4si",
+  blog: "#",
+  projects: "#",
+};
